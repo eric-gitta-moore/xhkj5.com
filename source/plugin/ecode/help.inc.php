19ebaeca02d43a02ff1178eb55c3a5813f285eea
Eric Moore <EricGittaMoore@duck.com>
2023-10-31 14:03:23 +0800

    chore: backup

---
+++ source/plugin/ecode/help.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+	exit('Access Denied');
+}
+
+?>
+
+<iframe frameBorder=0 width="100%" height="600" src="http://www.xhkj5.com/"></iframe> 
